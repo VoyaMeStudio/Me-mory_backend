c71f60f9a9a94ee817e254e73e69c215f14657e9
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-11-13 00:25:46 +0900

    refactor(logout): 로그아웃 코드 중복 제거 및 로그아웃 테스트 수정

--- src/main/java/zim/tave/memory/service/SettingService.java
+++ src/main/java/zim/tave/memory/service/SettingService.java
@@ -16,18 +16,6 @@ public class SettingService {
     private final VisitedCountryRepository visitedCountryRepository;
     private final DiaryRepository diaryRepository;
 
-    @Transactional
-    public void logout(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found"));
-
-        if (!user.isStatus()) {
-            throw new RuntimeException("User already logged out");
-        }
-        user.setStatus(false); //로그아웃 시 status false로 설정
-
-        //프론트에서 accessToken 삭제
-    }
 
     @Transactional
     public void deleteAccount(Long userId) {
