3b48e1903be32a564fab9ed03f2fa69e85145723
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2026-03-28 13:10:42 +0900

    fix: base-url 수정

--- src/main/java/zim/tave/memory/service/StickerService.java
+++ src/main/java/zim/tave/memory/service/StickerService.java
@@ -2,6 +2,7 @@ package zim.tave.memory.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -17,6 +18,16 @@ public class StickerService {
     private final UserStickerRepository userStickerRepository;
     private final S3Client s3Client;
     private final RestTemplate restTemplate;
+
+    @Value("${ai.server.url}")
+    private String aiServerUrl;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.base-url}")
+    private String s3BaseUrl;
+
 
 
 }
