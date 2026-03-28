e6941c65de3fa2c34e067cabbf3553d8d546e5ff
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2026-03-28 12:54:46 +0900

    feat: Sticker 생성, 리스트 응답 dto 구현

--- src/main/java/zim/tave/memory/repository/UserStickerRepository.java
+++ src/main/java/zim/tave/memory/repository/UserStickerRepository.java
@@ -1,4 +1,11 @@
 package zim.tave.memory.repository;
 
-public class UserStickerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import zim.tave.memory.domain.UserSticker;
+
+import java.util.List;
+
+public interface UserStickerRepository extends JpaRepository<UserSticker, Long> {
+
+    List<UserSticker> findByUserId(Long userId);
 }
